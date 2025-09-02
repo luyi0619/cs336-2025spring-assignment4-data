81c66997103e382449ed299eca577cff85049290
Yi Lu <luyi0619@gmail.com>
2025-09-02 04:39:54 +0000

    add large file

--- cs336_data/langid.py
+++ cs336_data/langid.py
@@ -5,7 +5,7 @@ from typing import Any
 
 
 def identify_language(text: str) -> tuple[Any, float]:
-  model = fasttext.load_model('/content/assignment4-data/cs336_data/lid.176.bin')
+  model = fasttext.load_model('/content/cs336-2025spring-assignment4-data/cs336_data/lid.176.bin')
   text = text.replace('\n', '')
   lang, value = model.predict(text)
   return (lang[0][-2:], value[0])
